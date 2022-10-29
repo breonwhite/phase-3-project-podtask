317412b27adf00269bd2dbfc58b20faf2c185732
Breon White <bdwhite92@gmail.com>
2022-10-29 16:23:42 -0400

    Add podcast controller

---
+++ app/controllers/podcasts_controller.rb
@@ -0,0 +1,9 @@
+class PodcastsController < ApplicationController
+    get "/podcasts" do
+        "Here is the list of podcasts"
+    end
+
+
+
+
+end
--- config.ru
+++ config.ru
@@ -12,4 +12,5 @@ end
 use Rack::JSONBodyParser
 
 # Our application
+use PodcastsController
 run ApplicationController
