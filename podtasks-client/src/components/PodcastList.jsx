22a52bff58a151afe1e03b980f44d16423953952
Breon White <bdwhite92@gmail.com>
2022-10-31 13:03:11 -0400

    Add data fecth function for Podcast List

--- podtasks-client/src/components/PodcastList.jsx
+++ podtasks-client/src/components/PodcastList.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 const PodcastList = () => {
-  return (
+    const [ podcasts, setPodcasts ] = useState([])
+    
+    useEffect(() => {
+        async function fetchData() {
+            const resp = await fetch('http://localhost:9292/podcasts')
+            const data = await resp.json();
+            setPodcasts(data);
+        }
+        fetchData();
+    }, [])
+  
+    return (
     <div>PodcastList</div>
   )
 }
