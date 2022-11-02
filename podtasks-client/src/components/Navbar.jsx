c1ea6d1c3a7363192a97167aa96c9640504d06d9
Breon White <bdwhite92@gmail.com>
2022-11-02 16:17:43 -0400

    Update navbar using Material Ui

--- podtasks-client/src/components/Navbar.jsx
+++ podtasks-client/src/components/Navbar.jsx
@@ -1,16 +1,43 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import PodcastsIcon from '@mui/icons-material/Podcasts';
+
 const Navbar = () => {
+    
   return (
-    <div>
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/podcasts/new">Add Podcast</Link></li>
-            <li><Link to="/podcasts">Podcasts</Link></li>
-            <li><Link to="/tasks">Tasks</Link></li>
-        </ul>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar disableGutters>
+        <PodcastsIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            PODTASK
+          </Typography>
+          <Button color="inherit" href="/podcasts/new">Add Podcast</Button>
+          <Button color="inherit" href="/podcasts">Manage Podcasts</Button>
+          <Button color="inherit" href="/tasks">All Tasks</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   )
 }
 
