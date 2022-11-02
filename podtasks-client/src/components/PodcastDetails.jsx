deac1d320ab07d6effb53ea20de058285c16b064
Breon White <bdwhite92@gmail.com>
2022-11-01 22:29:23 -0400

    Add Podcast details show page

---
+++ podtasks-client/src/components/PodcastDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PodcastDetails = ({ podcast }) => {
+  return (
+    <div><h1>Podcast Details</h1> <br/>
+    {/* { podcast.topic } */}
+    </div>
+  )
+}
+
+export default PodcastDetails
