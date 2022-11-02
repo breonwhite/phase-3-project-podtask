4923c5a1cffadd71551d60557b3201add9aa69f2
Breon White <bdwhite92@gmail.com>
2022-11-01 22:21:20 -0400

    Update Podcast Card to include tasks

--- podtasks-client/src/components/PodcastCard.jsx
+++ podtasks-client/src/components/PodcastCard.jsx
@@ -3,13 +3,27 @@ import { Link } from 'react-router-dom'
 
 
 const PodcastCard = ({ podcast }) => {
+    const todoList = podcast.tasks
+
   return (
-    <li>
+    <ul>
+        <li>
         <Link to={`/podcasts/${podcast.id}`}>
             { podcast.topic }
         </Link>
-    </li>
+        Description: { podcast.description } <br/>
+        Guest Speaker: { podcast.guest } <br/>
+        Expected Release Date: { podcast.release_date } <br/>
+        Todos: { todoList.map((todo, index) => (
+            <p key={index}>
+                { todo.to_do } - { todo.todo_status }
+            </p>
+        )
+        ) }
+        </li>
+    </ul>
   )
 }
 
 export default PodcastCard
+
