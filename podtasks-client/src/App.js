f5f505cb6e4d1c097291d0780e8cec96a2ee9ecb
Breon White <bdwhite92@gmail.com>
2022-11-06 13:37:53 -0500

    Add route for podcast edit page

--- podtasks-client/src/App.js
+++ podtasks-client/src/App.js
@@ -8,9 +8,9 @@ import NewPodcast from './components/NewPodcast';
 import PodcastDetails from './components/PodcastDetails';
 import TaskList from './components/TaskList';
 import PageNotFound from './components/PageNotFound';
+import PodcastEdit from './components/PodcastEdit';
 
 function App() {
-
 
   return (
     <Router>
@@ -21,6 +21,7 @@ function App() {
         <Route path="/podcasts" element={ <PodcastList /> } />
         <Route path="/podcasts/new" element={ <NewPodcast /> } />
         <Route path="/podcasts/:id" element={ <PodcastDetails /> } />
+        <Route path="/podcasts/:id/edit" element={ <PodcastEdit /> } />
         <Route path="/tasks" element={ <TaskList /> } />
         <Route element={ <PageNotFound /> } />
       </Routes>
