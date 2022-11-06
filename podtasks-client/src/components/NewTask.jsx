174243d3957d2dac12faa3e2df709b71c1ccd034
Breon White <bdwhite92@gmail.com>
2022-11-06 14:27:09 -0500

    Adjust form for new task

--- podtasks-client/src/components/NewTask.jsx
+++ podtasks-client/src/components/NewTask.jsx
@@ -1,17 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom'
-import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
-import IconButton from '@mui/material/IconButton';
-import AddBoxIcon from '@mui/icons-material/AddBox';
 import AddIcon from '@mui/icons-material/Add';
+import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
 
 const NewTask = ({ podcast }) => {
     const [ task, setTask ] = useState("");
-    const [loading, setLoading] = useState(true);
-    const { podcastId } = useParams();
 
     const handleSubmit = async e => {
         e.preventDefault();
@@ -27,20 +23,18 @@ const NewTask = ({ podcast }) => {
         }
         await fetch('http://localhost:9292/tasks', options);
         setTask("")
-        // redirect to Podcast List
-        // navigate("/podcasts");
     }
   
     return (
     <form onSubmit={ handleSubmit } >
             <Stack direction="row" spacing={2} sx={{ width: '100%', bgcolor: 'background.paper' }}>
-            <TextField id={ task } label="Add a new task" variant="standard" value={ task } onChange={ e => setTask( e.target.value ) } autoFocus={ true } sx={{ width: '85%' }} />
+            <TextField id={ task } label="Enter a task to add to your list" variant="standard" value={ task } onChange={ e => setTask( e.target.value ) } autoFocus={ true } sx={{ width: '85%' }} />
             <Button
                 sx={{ width: '10%' }}
                 variant="contained"
                 type="submit"
                 size="large"
-                startIcon={<AddIcon />}>
+                startIcon={<PlaylistAddIcon />}>Add
             </Button>
             </Stack>
     </form>
