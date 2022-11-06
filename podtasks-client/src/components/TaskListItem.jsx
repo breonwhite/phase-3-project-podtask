14d0e4c052fbb3322bcb749f2a3c09d035d8f48c
Breon White <bdwhite92@gmail.com>
2022-11-06 14:21:29 -0500

    Strikethrough todo when task is marked as completed

--- podtasks-client/src/components/TaskListItem.jsx
+++ podtasks-client/src/components/TaskListItem.jsx
@@ -1,29 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Checkbox from '@mui/material/Checkbox';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import VerifiedIcon from '@mui/icons-material/Verified';
-
-import DeleteIcon from '@mui/icons-material/Delete';
-import SendIcon from '@mui/icons-material/Send';
-import Stack from '@mui/material/Stack';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import Divider from '@mui/material/Divider';
-
-
 
 const TaskListItem = ({ task, completeTask, handleRemove }) => {
 
-const handleComplete = (value) => {
-    completeTask(value)
-    //setComplete(true)
-}
-  
+    const handleComplete = (value) => {
+        completeTask(value)
+    }
+
 return (
     <ListItem
         secondaryAction={
@@ -36,7 +23,7 @@ return (
             Task Complete
         </Button>
         } >
-    <ListItemText id={task.index} primary={<s>{task.to_do}</s>} 
+    <ListItemText id={task.index} primary={task.todo_status == "incomplete" ? task.to_do : <s>{task.to_do}</s>}
         secondary={
             <Button size="small" variant="text" onClick={ () => handleRemove( task.id ) }>
                 DELETE</Button>
