ee9eac51396e35fb868d8286680b4fa5e75b0cc4
Breon White <bdwhite92@gmail.com>
2022-11-06 13:54:28 -0500

    Cleanup code for podcast cards

--- podtasks-client/src/components/PodcastCard.jsx
+++ podtasks-client/src/components/PodcastCard.jsx
@@ -1,23 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import Box from '@mui/material/Box';
-import { styled } from '@mui/material/styles';
+
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import Container from '@mui/material/Container';
-import SettingsIcon from '@mui/icons-material/Settings';
 import EditIcon from '@mui/icons-material/Edit';
 import PodcastsIcon from '@mui/icons-material/Podcasts';
 import Grid from '@mui/material/Grid';
@@ -32,19 +21,16 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
 
-function createData(todo, todo_status) {
-    return { todo, todo_status };
-  }
-
-
 const PodcastCard = ({ podcast }) => {
     const todoList = podcast.tasks
 
+    // capitalizes only first letter of word in string
     const capitalizeFirst = str => {
         return str.charAt(0).toUpperCase() + str.slice(1);
       }
     
-    const toTitleCase = (str) => {
+      // capitalizes first letter of *each* word in string
+      const toTitleCase = (str) => {
         return str.replace(/\w\S*/g, function(txt){
             return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
         });
@@ -60,34 +46,36 @@ const PodcastCard = ({ podcast }) => {
                 </Avatar>
             }
             action={
-          <IconButton href={`/podcasts/${podcast.id}`} aria-label="edit">
-            <EditIcon />
-          </IconButton>
+                <IconButton href={`/podcasts/${podcast.id}`} aria-label="edit">
+                    <EditIcon />
+                </IconButton>
             }
-            subheader={<Typography sx={{ fontSize: 14 }} color="text.secondary">
-            Featuring Guest: { podcast.guest }
-        </Typography>}
-            title={<Typography variant="h5" component="div">
-            { toTitleCase(podcast.topic) }
-        </Typography>}>
-            <Typography sx={{ fontSize: 14 }} color="text.secondary">
-                Topic
-            </Typography>
+            subheader={
+                <Typography sx={{ fontSize: 14 }} color="text.secondary">
+                    Featuring Guest: { podcast.guest }
+                </Typography>}
+            title={
+                <Typography variant="h5" component="div">
+                    { toTitleCase(podcast.topic) }
+                </Typography>}>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary">
+                    Topic
+                </Typography>
         </CardHeader>
         <CardContent>
-        <Typography variant="subtitle2">
-            Description
-        </Typography>
-        <Typography variant="body2">
-            { podcast.description }
-        </Typography>
-        <br />
-        <Typography variant="subtitle2">
-            Release Date
-        </Typography>
-        <Typography variant="body2">
-        { podcast.release_date }
-        </Typography>
+            <Typography variant="subtitle2">
+                Description
+            </Typography>
+            <Typography variant="body2">
+                { podcast.description }
+            </Typography>
+            <br />
+            <Typography variant="subtitle2">
+                Release Date
+            </Typography>
+            <Typography variant="body2">
+                { podcast.release_date }
+            </Typography>
         </CardContent>
         <CardContent>
             <TableContainer component={Paper} elevation={24}>
